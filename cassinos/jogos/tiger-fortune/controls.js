2d2faca78058dd0e164d9fa4c12d3264d68a8a82
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-10 02:25:08 -0300

    s

---
+++ cassinos/jogos/tiger-fortune/controls.js
@@ -0,0 +1,11 @@
+const image = document.getElementById('image');
+const clickSound = document.getElementById('click-sound');
+
+image.addEventListener('click', () => {
+  image.classList.add('rotate-fast');
+  clickSound.play();
+  // Após 3 segundos, volta à rotação normal
+  setTimeout(() => {
+    image.classList.remove('rotate-fast');
+  }, 2000);
+});
