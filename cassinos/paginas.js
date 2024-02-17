9a0d1ff91a66dc5250f6ab6d5814a5b9becbdf31
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-17 03:54:04 -0300

    s

--- cassinos/paginas.js
+++ cassinos/paginas.js
@@ -7,11 +7,14 @@ function showSection(sectionNumber) {
     // Exibe a seção desejada
     var sectionToShow = document.getElementById('section' + sectionNumber);
     sectionToShow.style.display = 'flex';
-    sectionToShow.classList.add('active1');
+    sectionToShow.classList.add('active');
     // Remove a classe 'active' de todas as seções, exceto da seção atual
     sections.forEach(function(section) {
       if (section !== sectionToShow) {
-        section.classList.remove('active1');
+        section.classList.remove('active');
       }
     });
   }
+
+  
+  
