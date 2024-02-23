5fc3132d156888bf5ab89bec77d1b21c6fc4924e
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-23 01:54:43 -0300

    s

--- rio40graus/telamenu.js
+++ rio40graus/telamenu.js
@@ -1,15 +1,21 @@
+var burg = document.querySelector('#burg')
+var fechar = document.querySelector('#fechar')
+
 function clickMenu() { 
-        if (itens.style.display == 'block') {
-            itens.style.display = 'none'
-        } else {
-            itens.style.display = 'block'
-        }
 
-
-    }
+             itens.style.display = 'block'
+             burg.style.display = 'none'
+             fechar.style.display = 'block'   
+}
+function closeMenu() {
+    itens.style.display = "none"
+    fechar.style.display = 'none'
+    burg.style.display = 'block'
+   
+}
 
 function mudouTamanho() {
-            if (window.innerWidth >= 768) {
+            if (window.innerWidth >= 600) {
                 itens.style.display = 'inline-block'
             } else {
                 itens.style.display = 'none'
