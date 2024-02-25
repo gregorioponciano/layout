99b2fc1764496c01a62e06835a1d9fcba2a537df
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-02-25 12:33:55 -0300

    s

--- cassinos/alert.js
+++
@@ -1,7 +0,0 @@
-// Exibe uma mensagem de boas-vindas ao jogador
-//function showWelcomeMessage() {
-    alert("Bem-vindo ao Cassino Online!");
-  }
-  https://www.kto.com/pt/registro/
-  // Chama a função showWelcomeMessage ao carregar a página
-  window.onload = showWelcomeMessage;
