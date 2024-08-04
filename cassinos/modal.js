96e6559ca1c05beb88eb718ebb33d8b293674025
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-08-04 12:05:49 -0300

    s

--- cassinos/modal.js
+++ cassinos/modal.js
@@ -1,3 +1,4 @@
+//modal perfil
 function openModal() {
     document.getElementById('myModal').style.display = 'block';
 }
@@ -35,5 +36,30 @@ window.onclick = function(event) {
             const modalScreen = document.getElementById('fullscreenModal');
             if (event.target == modalScreen) {
                 closeFullscreenModal();
+            }
+        }
+
+        //modal fullscreen deposito
+
+        function openGrayModal() {
+            const modalDeposito = document.getElementById('grayModal');
+            modalDeposito.style.display = 'block';
+            setTimeout(() => {
+                modalDeposito.classList.add('show');
+            }, 10); // Delay necessário para acionar a transição
+        }
+
+        function closeGrayModal() {
+            const modalDeposito = document.getElementById('grayModal');
+            modalDeposito.classList.remove('show');
+            setTimeout(() => {
+                modalDeposito.style.display = 'none';
+            }, 500); // Tempo deve coincidir com o da transição
+        }
+
+        window.onclick = function(event) {
+            const modalDeposito = document.getElementById('grayModal');
+            if (event.target == modalDeposito) {
+                closeGrayModal();
             }
         }
