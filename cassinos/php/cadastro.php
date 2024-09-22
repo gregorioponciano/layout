767632360d5fb70ab4ca28c206c31a016b0be4eb
gregorioponciano <gregorioponci.91@gmail.com>
2024-09-22 14:27:09 -0300

    s

---
+++ cassinos/php/cadastro.php
@@ -0,0 +1,19 @@
+<?php
+include 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Cadastro realizado com sucesso!";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
