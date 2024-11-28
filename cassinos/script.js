703ed61c1caad45ee6ea1ee0a6c56388f5550a93
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-11-28 01:21:47 -0300

    s

--- cassinos/script.js
+++ cassinos/script.js
@@ -1,22 +1,22 @@
-        // Função para abrir um modal
-        function openModal(modalId) {
-            closeAllModals();
-            document.getElementById(`modal-${modalId}`).classList.add('active');
-        }
+     // Função para abrir um modal
+     function openModal(modalId) {
+        closeAllModals();
+        document.getElementById(`modal-${modalId}`).classList.add('active');
+    }
 
-        // Função para fechar um modal específico
-        function closeModal(modalId) {
-            document.getElementById(`modal-${modalId}`).classList.remove('active');
-        }
+    // Função para fechar um modal específico
+    function closeModal(modalId) {
+        document.getElementById(`modal-${modalId}`).classList.remove('active');
+    }
 
-        // Função para fechar todos os modais
-        function closeAllModals() {
-            document.querySelectorAll('.modal').forEach(modal => {
-                modal.classList.remove('active');
-            });
-        }
+    // Função para fechar todos os modais
+    function closeAllModals() {
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.classList.remove('active');
+        });
+    }
 
-        // Função para voltar ao conteúdo principal do body
-        function returnToBody() {
-            closeAllModals()
-        }
+    // Função para voltar ao conteúdo principal do body
+    function returnToBody() {
+        closeAllModals()
+    }
