7b7138f15c4940388422802c2cc50afd2680682d
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-12-08 20:06:07 -0300

    ghj

--- cassinos/public/js/afiliado.js
+++ cassinos/public/js/afiliado.js
@@ -18,3 +18,29 @@
             // Adiciona a classe 'active' ao botão correspondente
             document.getElementById(`btn-${conteudoId}`).classList.add('active');
         }
+
+
+        // funçao para copiar o link de indicaçao 
+
+        function copyText(element) {
+            const textToCopy = element.getAttribute('data-text');
+            
+            // Cria um elemento temporário para copiar o texto
+            const tempInput = document.createElement('textarea');
+            tempInput.value = textToCopy;
+            document.body.appendChild(tempInput);
+            
+            // Seleciona e copia o texto
+            tempInput.select();
+            document.execCommand('copy');
+            
+            // Remove o elemento temporário
+            document.body.removeChild(tempInput);
+            
+            // Mostra mensagem de sucesso
+            const message = document.getElementById('message');
+            message.style.display = 'block';
+            setTimeout(() => {
+                message.style.display = 'none';
+            }, 2000);
+        }
