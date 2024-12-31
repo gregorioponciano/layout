1de583c32ad74c6b6566af2c6c8be370b67bd7f8
Gregório Ponciano <gregorioponci.91@gmail.com>
2024-12-31 09:17:24 -0300

    s

--- rio40graus/telamenu.js
+++ rio40graus/telamenu.js
@@ -2,30 +2,23 @@ var burg = document.querySelector('#burg');
 var fechar = document.querySelector('#fechar');
 var aparecer = document.querySelector('#section-menu');
 
-// Abrir o menu
 function clickMenu() {
-    aparecer.style.display = 'block';
-    burg.style.display = 'none';
-    fechar.style.display = 'block';
-}
-
-// Fechar o menu
-function closeMenu() {
-    aparecer.style.display = 'none';
-    fechar.style.display = 'none';
-    burg.style.display = 'block';
+            
+    if ( aparecer.style.display == 'block') {
+        aparecer.style.display = 'none'
+    } else {
+        aparecer.style.display = 'block'
+    }
 }
 
 // Ajustar menu ao redimensionar a tela
 function mudouTamanho() {
     if (window.innerWidth >= 768) {
         aparecer.style.display = 'block'; // Mostrar o menu em telas maiores
-        burg.style.display = 'none'; // Esconder o ícone do menu burger
-        fechar.style.display = 'none'; // Esconder o ícone de fechar
+        burg.style.display = 'none'; // Esconder o ícone do menu burger      
     } else {
         aparecer.style.display = 'none'; // Esconder o menu em telas menores
-        burg.style.display = 'block'; // Mostrar o ícone do menu burger
-        fechar.style.display = 'none'; // Esconder o ícone de fechar
+        burg.style.display = 'block'; // Mostrar o ícone do menu burger    
     }
 }
 
