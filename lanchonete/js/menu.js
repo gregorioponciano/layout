1eceecde5dab81653a8515952d9fa9d4047ef512
gregorio <gregorioponci.91@gmail.com>
2025-09-14 18:31:30 -0300

    sim arrumei o menu da lanchonete

--- lanchonete/js/menu.js
+++ lanchonete/js/menu.js
@@ -1,22 +1,12 @@
 function toggleMobileMenu() {
   const nav = document.querySelector(".navigation-header");
-
-  // se não existir o botão de fechar, cria
-  if (!nav.querySelector(".close-btn")) {
-    const closeItem = document.createElement("li");
-    closeItem.classList.add("close-btn");
-    closeItem.innerHTML = "&times;"; // X estilizado
-    closeItem.onclick = () => nav.classList.remove("open");
-    nav.prepend(closeItem);
-  }
-
   nav.classList.toggle("open");
 }
 
 // Fecha menu se clicar fora
 document.addEventListener("click", (e) => {
   const nav = document.querySelector(".navigation-header");
-  const logo = document.querySelector(".nav-logo span");
+  const logo = document.querySelector(".nav-logo i");
 
   if (
     nav.classList.contains("open") &&
